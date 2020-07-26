9b643c41b3d20b5ddc0104e9652066b82eae8f90
Mukesh Gupta <mukeshgupta202000@gmail.com>
2020-07-26 17:47:03 +0530

    :)

--- client/src/components/Post/Post.js
+++ client/src/components/Post/Post.js
@@ -116,7 +116,7 @@ class Post extends Component {
                             </div>
                         </div>
                         <div className="row post-body">
-                            <p class="post-body-text">{post.body}</p>
+                            <p className="post-body-text">{post.body}</p>
                         </div>
                         <div className="row post-footer">
                             <div className="post-btn-container">
